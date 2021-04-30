be76702139d8f0ea3d0265b7c49eb44d399ce264
Denis O <denis.o@linux.com>
2021-04-30 13:54:14 +0300

    Imported basic spring boot project

---
+++ src/main/java/com/brk/timeshift/TimeshiftApplication.java
@@ -0,0 +1,13 @@
+package com.brk.timeshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brk/timeshift/TimeshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brk.timeshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
